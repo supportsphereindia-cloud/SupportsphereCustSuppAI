a6a0f9919c190de1d386fb9430967a9a63868810
Aaryan Yerunkar <supportsphereindia@gmail.com>
2026-09-01 15:16:53 +0530

    Prepare server for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("=================================");
   console.log("🚀 SupportSphere Backend Started");
-  console.log(`🌐 Server Running : http://localhost:${PORT}`);
+  console.log(`🌐 Server Running on port ${PORT}`);
   console.log(`🟢 Environment    : ${process.env.NODE_ENV}`);
   console.log("=================================");
 });
